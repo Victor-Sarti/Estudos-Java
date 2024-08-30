ec7f067e3101b4f4ad925adafb95b83856dd107b
victor-sarti <sartivictor24@gmail.com>
2024-08-30 09:31:37 -0300

    create class TesteVetorAnimal array of dogs

---
+++ src/Animal/TesteVetorAnimal.java
@@ -0,0 +1,16 @@
+package Animal;
+
+public class TesteVetorAnimal {
+	public static void main(String[] args) {
+		
+		
+		Cachorro[] cachorros = new Cachorro[10];
+		for (int i = 0; i < cachorros.length; i++) {
+			cachorros[i] = new Cachorro("Cachorro" + i, "PitBull");
+		}
+		for (int i = 0; i < cachorros.length; i++) {
+				cachorros[i].latir(3);
+		}
+	}
+
+}
