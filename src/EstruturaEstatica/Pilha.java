a81781a5c8b46549a91710065718395bb18f0f08
victor-sarti <sartivictor24@gmail.com>
2024-09-03 10:59:14 -0300

    Create metodo and teste desempilhar

--- src/EstruturaEstatica/Pilha.java
+++ src/EstruturaEstatica/Pilha.java
@@ -1,24 +1,33 @@
 package EstruturaEstatica;
 
 public class Pilha<T> extends aula4VetorObject<T> {
-	
-	public Pilha () {
+
+	public Pilha() {
 		super();
 	}
-	
+
 	public Pilha(int capacidade) {
 		super(capacidade);
 	}
-	
-	public void empilha (T elemento) {
+
+	public void empilha(T elemento) {
 		super.adiciona(elemento);
 	}
-	
+
 	public T topo() {
-		if(this.estaVazia()) {
+		if (this.estaVazia()) {
 			return null;
-		}else {
-			return this.elementos[tamanho-1];
+		} else {
+			return this.elementos[tamanho - 1];
 		}
+	}
+
+	public T desempilhar() {
+		if (this.estaVazia()) {
+			return null;
+		}
+		T elemento = this.elementos[tamanho - 1];
+		tamanho--;
+		return elemento;
 	}
 }
--- src/EstruturaEstatica/TestePilha.java
+++ src/EstruturaEstatica/TestePilha.java
@@ -3,11 +3,11 @@ package EstruturaEstatica;
 public class TestePilha {
 	public static void main(String[] args) {
 		Pilha<Integer> pilha = new Pilha<Integer>();
-		System.out.println(pilha.topo());
 		pilha.empilha(1);
 		pilha.empilha(2);
 		pilha.empilha(3);
-		System.out.println(pilha.topo());
+		System.out.println(pilha);
+		System.out.println("Desempilhar elemento" +pilha.desempilhar());
 		System.out.println(pilha);
 	}
 
