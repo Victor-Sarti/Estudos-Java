30b986005c29c411a1e4560a93cd5d0636abed37
victor-sarti <sartivictor24@gmail.com>
2024-09-04 09:03:48 -0300

    class FornecedorEmpresa

---
+++ src/PI/FornecedorEmpresa.java
@@ -0,0 +1,32 @@
+package PI;
+
+public class FornecedorEmpresa {
+
+	private String ie;
+	private String cnpj;
+	
+	public void FornecedorEmpresa() {
+		
+	}
+	
+	public void FornecedorEmpresa(String nome, String fone, String ie, String cnpj) {
+		
+	}
+
+	public String getIe() {
+		return ie;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setIe(String ie) {
+		this.ie = ie;
+	}
+
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+}
