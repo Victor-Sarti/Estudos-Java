6bd9e30f0403854cb23454f89742452e53b7aa6f
victor-sarti <sartivictor24@gmail.com>
2024-09-04 08:52:11 -0300

    class HerancaFornecedor

--- src/PI/HerancaFornecedor.java
+++ src/PI/HerancaFornecedor.java
@@ -1,5 +1,32 @@
 package PI;
 
 public class HerancaFornecedor {
+	private String nome;
+	private String fone;
+	
+	public void Fornecedor(){
+		
+	}
+	
+	public void Fornecedor(String nome, String fone) {
+		
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	
+	
 }
