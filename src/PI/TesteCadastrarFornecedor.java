719ea013406a68f67318205c88991bb246e7f6da
victor-sarti <sartivictor24@gmail.com>
2024-09-04 11:00:18 -0300

    ajust metodo main

--- src/PI/TesteCadastrarFornecedor.java
+++ src/PI/TesteCadastrarFornecedor.java
@@ -4,28 +4,33 @@ import javax.swing.JOptionPane;
 
 public class TesteCadastrarFornecedor {
 	public static void main(String[] args) {
-		
-		String valores[] = { "Pessoa", "Empresa" };
-		String continuar;
+		int resp = 0;
 		do {
-		Object escolha = JOptionPane.showInputDialog(null, "Qual Fornecedor deseja Cadastrar?", "Cadastro de Pessoa", 3,
-				null, valores, valores[0]);
-		HerancaFornecedor f = null;
-		if (escolha.equals("Pessoa")) {
-			String cpf, rg;
-			cpf = JOptionPane.showInputDialog("Digite o CPF ");
-			
-			f = new FornecedorPessoa(JOptionPane.showInputDialog("Digite o Nome da Pessoa"),
-					JOptionPane.showInputDialog("Digite o Telefone "), cpf,
-					JOptionPane.showInputDialog("Digite o RG "));
-		} else if (escolha.equals("Empresa")) {
-			f = new FornecedorEmpresa(JOptionPane.showInputDialog("Digite o Nome da Empresa"),
-					JOptionPane.showInputDialog("Digite o Telefone "), JOptionPane.showInputDialog("Digite o CNPJ "),
-					JOptionPane.showInputDialog("Digite a IE "));
-		}
-		JOptionPane.showConfirmDialog(null, f);
-		 continuar = JOptionPane.showInputDialog("Deseja cadastrar outro fornecedor? (sim/não)");
-        } while (continuar.equalsIgnoreCase("sim"));
-		 JOptionPane.showMessageDialog(null, "Cadastro de fornecedores concluído.");
+			int qtd = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade a cadastrar:"));
+
+			HerancaFornecedor f[] = new HerancaFornecedor[qtd];
+
+			for (int i = 0; i < qtd; i++) {
+				String valores[] = { "Pessoa", "Empresa" };
+				Object escolha = JOptionPane.showInputDialog(null, "Qual Pessoa deseja Cadastrar?",
+						"Cadastro de Fornecedor", JOptionPane.QUESTION_MESSAGE, null, valores, valores[0]);
+				if (escolha.equals("Pessoa")) {
+					String cpf, rg;
+					cpf = JOptionPane.showInputDialog("Digite o CPF ");
+					rg = JOptionPane.showInputDialog("Digite o RG ");
+					f[i] = new FornecedorPessoa(JOptionPane.showInputDialog("Digite o Nome da Pessoa"),
+							JOptionPane.showInputDialog("Digite o Telefone "), cpf, rg);
+				} else if (escolha.equals("Empresa")) {
+					f[i] = new FornecedorEmpresa(JOptionPane.showInputDialog("Digite o Nome da Empresa"),
+							JOptionPane.showInputDialog("Digite o Telefone "),
+							JOptionPane.showInputDialog("Digite o CNPJ "), JOptionPane.showInputDialog("Digite a IE "));
+				}
+			}
+
+			for (int i = 0; i < qtd; i++) {
+				System.out.println(f[i]);
+			}
+			resp = JOptionPane.showConfirmDialog(null, "Cadastrar Outros?");
+		} while (resp == 0);
 	}
 }
