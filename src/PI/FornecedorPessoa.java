c875865ed968d32f1f19a2bb449f408d792871ef
victor-sarti <sartivictor24@gmail.com>
2024-09-04 08:56:34 -0300

    class FornecedorPessoa

---
+++ src/PI/FornecedorPessoa.java
@@ -0,0 +1,36 @@
+package PI;
+
+public class FornecedorPessoa {
+
+	private String rg;
+	private String cpf;
+	
+	public void FornecedorPessoa() {
+		
+	}
+	
+	public void FornecedorPessoa(String nome, String fone, String rg, String cpf) {
+		
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
+
