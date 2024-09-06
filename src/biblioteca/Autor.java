56a7c940cc828c79791634546a61bc9ba373bfd2
Victor-Sarti <sartivictor24@gmail.com>
2024-09-06 08:41:41 -0300

    create class Autor

---
+++ src/biblioteca/Autor.java
@@ -0,0 +1,33 @@
+package biblioteca;
+
+public class Autor {
+	private int id;
+	private String nome;
+	private String generoLiterario;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getGeneroLiterario() {
+		return generoLiterario;
+	}
+	
+	public void setGeneroLiterario(String generoLiterario) {
+		this.generoLiterario = generoLiterario;
+	}
+	
+
+}
--- src/biblioteca/Livro.java
+++ src/biblioteca/Livro.java
@@ -1,5 +1,51 @@
 package biblioteca;
 
 public class Livro {
-
+	private String isbn;
+	private String titulo;
+	private String dataLancamento;
+	private String edicao;
+	private String numPaginas;
+	
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDataLancamento() {
+		return dataLancamento;
+	}
+	
+	public void setDataLancamento(String dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+	
+	public String getEdicao() {
+		return edicao;
+	}
+	
+	public void setEdicao(String edicao) {
+		this.edicao = edicao;
+	}
+	
+	public String getNumPaginas() {
+		return numPaginas;
+	}
+	
+	public void setNumPaginas(String numPaginas) {
+		this.numPaginas = numPaginas;
+	}
+	
 }
