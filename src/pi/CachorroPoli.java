0cc13425be8ce90e38876ee96b8f44e283aee5d3
Victor-Sarti <sartivictor24@gmail.com>
2024-09-11 11:16:21 -0300

    create and class cachorroPoli

---
+++ src/pi/CachorroPoli.java
@@ -0,0 +1,9 @@
+package pi;
+
+public class CachorroPoli extends bichoPoli {
+	 
+		public void fala() {
+		System.out.println("Au au");	
+		}
+		
+}
--- src/pi/bichoPoli.java
+++ src/pi/bichoPoli.java
@@ -1,5 +1,8 @@
 package pi;
 
 public class bichoPoli {
-
+public void fala() {
+	System.out.println("Som não identificado");
+}
+	
 }
