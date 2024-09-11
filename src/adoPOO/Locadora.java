4f63f0e3fb82dc302966ba3b690d5455c9e9de80
Victor-Sarti <sartivictor24@gmail.com>
2024-09-11 10:56:41 -0300

    create class Locadora

---
+++ src/adoPOO/Locadora.java
@@ -0,0 +1,40 @@
+package adoPOO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Locadora {
+
+	 private String nome;
+	    private List<Aluguel> alugueis;
+
+	    public Locadora(String nome) {
+	        this.nome = nome;
+	        this.alugueis = new ArrayList<>();
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public List<Aluguel> getAlugueis() {
+	        return alugueis;
+	    }
+
+	    public void adicionarAluguel(Aluguel aluguel) {
+	        this.alugueis.add(aluguel);
+	    }
+
+	    @Override
+	    public String toString() {
+	        return "Locadora{" +
+	               "nome='" + nome + '\'' +
+	               ", alugueis=" + alugueis +
+	               '}';
+	    }
+	
+}
