ea16a11957171accb5d935caf2327a9e1e087bbc
victor-sarti <sartivictor24@gmail.com>
2024-09-13 10:50:55 -0300

    upp janela

--- src/heranca/MinhaJanela.java
+++ src/heranca/MinhaJanela.java
@@ -1,11 +1,24 @@
 package heranca;
 
+import java.awt.Cursor;
+
+
 import javax.swing.JFrame;
 
 public class MinhaJanela extends JFrame {
+	
+
+	
 	public MinhaJanela() {
+	
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); //comando para fechar o programa ao fechar a janela
 		setBounds(100, 100, 500, 500); //comando para dar tamanho e coordenadas da janela 
+		setTitle("Minha Janela");
+		setResizable(false); // Proibe de aumentar o tamanho da janela 
+		setCursor(Cursor.HAND_CURSOR); // altera o cursor para mãozinha 
+		setExtendedState(MAXIMIZED_BOTH); // deixa a janela na tela inteira
+		
+		
 	}
 }
