e56211e2c4e264389eb81ef68700d02c1462bc06
victor-sarti <sartivictor24@gmail.com>
2024-09-13 10:58:25 -0300

    create button

--- src/heranca/MinhaJanela.java
+++ src/heranca/MinhaJanela.java
@@ -2,23 +2,28 @@ package heranca;
 
 import java.awt.Cursor;
 
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MinhaJanela extends JFrame {
-	
 
-	
+	private JButton meuBotao;
+
 	public MinhaJanela() {
-	
+		// botão
+		meuBotao = new JButton("Botão");
+		meuBotao.setBounds(10, 10, 150, 30);
+		// adicionando componentes
+		getContentPane().setLayout(null);
+		getContentPane().add(meuBotao);
+
 		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE); //comando para fechar o programa ao fechar a janela
-		setBounds(100, 100, 500, 500); //comando para dar tamanho e coordenadas da janela 
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // comando para fechar o programa ao fechar a janela
+		setBounds(100, 100, 500, 500); // comando para dar tamanho e coordenadas da janela
 		setTitle("Minha Janela");
-		setResizable(false); // Proibe de aumentar o tamanho da janela 
-		setCursor(Cursor.HAND_CURSOR); // altera o cursor para mãozinha 
+		setResizable(false); // Proibe de aumentar o tamanho da janela
+		setCursor(Cursor.HAND_CURSOR); // altera o cursor para mãozinha
 		setExtendedState(MAXIMIZED_BOTH); // deixa a janela na tela inteira
-		
-		
+
 	}
 }
