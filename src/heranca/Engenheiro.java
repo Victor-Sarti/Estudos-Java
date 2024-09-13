1f6cb8470367711e1bb0ccdba69e789cdfbff5bd
victor-sarti <sartivictor24@gmail.com>
2024-09-13 08:39:21 -0300

    create class Engenheiro

---
+++ src/heranca/Engenheiro.java
@@ -0,0 +1,15 @@
+package heranca;
+
+public class Engenheiro extends Funcionario{
+	private String numCrc;
+
+	public String getNumCrc() {
+		return numCrc;
+	}
+
+	public void setNumCrc(String numCrc) {
+		this.numCrc = numCrc;
+	}
+	
+
+}
---
+++ src/heranca/Funcionario.java
@@ -0,0 +1,40 @@
+package heranca;
+
+public class Funcionario {
+	private String matricula;
+	private String nome;
+	private double salario;
+	private String rg;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+}
