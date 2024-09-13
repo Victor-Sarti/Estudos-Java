dcb49ee793bde2fb12b97aaa5c2db854dc486e21
victor-sarti <sartivictor24@gmail.com>
2024-09-13 11:03:26 -0300

    upp button

--- src/heranca/MinhaJanela.java
+++ src/heranca/MinhaJanela.java
@@ -1,18 +1,24 @@
 package heranca;
 
 import java.awt.Cursor;
-
+import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MinhaJanela extends JFrame {
 
 	private JButton meuBotao;
+	private Font fonte;
 
 	public MinhaJanela() {
+
+		// fonte
+		fonte = new Font("Arial", Font.BOLD | Font.ITALIC, 20);
+
 		// botão
 		meuBotao = new JButton("Botão");
 		meuBotao.setBounds(10, 10, 150, 30);
+		meuBotao.setFont(fonte);
 		// adicionando componentes
 		getContentPane().setLayout(null);
 		getContentPane().add(meuBotao);
