4b89d752b54efa644da5098ae864322a0feea499
victor-sarti <sartivictor24@gmail.com>
2024-09-13 08:41:22 -0300

    create class Contador

---
+++ src/heranca/Contador.java
@@ -0,0 +1,14 @@
+package heranca;
+
+public class Contador extends Funcionario {
+	private String numCrc;
+
+	public String getNumCrc() {
+		return numCrc;
+	}
+
+	public void setNumCrc(String numCrc) {
+		this.numCrc = numCrc;
+	}
+
+}
