81d48f7b7ddaf4b2e94335e55782c06b9b560f9f
victor-sarti <sartivictor24@gmail.com>
2024-09-13 10:35:14 -0300

    create toString and equals Contador

--- src/heranca/Contador.java
+++ src/heranca/Contador.java
@@ -1,10 +1,42 @@
 package heranca;
 
+import java.util.Objects;
+
 public class Contador extends Funcionario {
 	public Contador() {
 		System.out.println("Passou pelo Contador");
 	}
 	
+	@Override
+	public String toString() {
+		
+		return this.getNome() + "- CRC:" + this.getNumCrc();
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(numCrc);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contador other = (Contador) obj;
+		return Objects.equals(numCrc, other.numCrc);
+	}
+
+
+
 	private String numCrc;
 
 	public String getNumCrc() {
