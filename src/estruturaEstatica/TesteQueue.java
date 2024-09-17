cd7ad188c307f39b9d49e9ba889bd18ef6d54792
victor-sarti <sartivictor24@gmail.com>
2024-09-17 10:29:57 -0300

    use metodo ja pronto TesteQueue

---
+++ src/estruturaEstatica/TesteQueue.java
@@ -0,0 +1,22 @@
+package estruturaEstatica;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TesteQueue {
+	public static void main(String[] args) {
+		Queue<Integer>fila = new LinkedList<Integer>();
+		
+		
+		
+		fila.add(1);
+		fila.add(2);
+		fila.add(3);
+		
+		System.out.println("Tamanho da Fila: " + fila.size());
+		System.out.println("Dados da Fila: " + fila.toString());
+		System.out.println(fila.peek());
+		
+	
+	}
+
+}
