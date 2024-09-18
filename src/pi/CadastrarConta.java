cc97b7e3012c19b7b93c67054a445d4bac74e3f2
victor-sarti <sartivictor24@gmail.com>
2024-09-18 08:55:07 -0300

    create metodo criarConta

--- src/pi/CadastrarConta.java
+++ src/pi/CadastrarConta.java
@@ -6,11 +6,16 @@ public class CadastrarConta {
 	public static void main(String[] args) {
 		Conta c = new Conta();
 		
+		criarConta(c);
+	}
+
+	private static void criarConta(Conta c) {
 		c.setNumero(Integer.parseInt( JOptionPane.showInputDialog("Digite o numero da Conta: ", 1)));
 		c.setSaldo(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do Deposito: ", 500)));
 		c.setLimite(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do Limite: ", 2000)));
 		c.setNome(JOptionPane.showInputDialog("Digite o Nome do Cliente: ", "Juan"));
 		System.out.println(c);
+		
 	}
 	
 	
