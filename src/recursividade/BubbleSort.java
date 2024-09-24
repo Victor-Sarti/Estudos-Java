493e60958dc451f8d202e1e93789973d36a86ecc
Victor-sarti <Sartivictor24@gmail.com>
2024-09-24 09:02:06 -0300

    metodo ordenado bubbleSort

--- src/recursividade/BubbleSort.java
+++ src/recursividade/BubbleSort.java
@@ -1,19 +1,34 @@
 package recursividade;
 
 public class BubbleSort {
-public static void main(String[] args) {
-	//criando vetor int com 10 posicoes 
-	
-	int [] vetor = new int [10];
-	
-	//inserindo valores no vetor
-	for(int i = 0; i < vetor.length; i++) {
-		vetor[i] = (int) (Math.random() * vetor.length);
+	public static void main(String[] args) {
+		// criando vetor int com 10 posicoes
+
+		int[] vetor = new int[10];
+
+		// inserindo valores no vetor
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = (int) (Math.random() * vetor.length);
+			// exibindo os valores do vetor
+			System.out.println(vetor[i]);
+
+		}
 		
-		//exibindo os valores do vetor
-		System.out.println(vetor[i]);
-			
-		
+		// metodos de ordenaçao BubbleSort
+		int aux;
+		for (int i = 0; i < vetor.length; i++) {
+			for (int j = i + 1; j < vetor.length; j++) {
+				if (vetor[i] > vetor[j]) {
+					aux = vetor[j];
+					vetor[j] = vetor[i];
+					vetor[i] = aux;
+				}
+			}
+
+		}
+		System.out.println("Vetor ordenado: ");
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.println(vetor[i]);
+		}
 	}
-}
 }
