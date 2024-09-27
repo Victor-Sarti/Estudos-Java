214f2d23803aacb6b818f6519c2393c57c6d82d4
Victor-sarti <Sartivictor24@gmail.com>
2024-09-27 11:25:38 -0300

    ajust class teste and finish

--- src/exception/TesteMatematica.java
+++ src/exception/TesteMatematica.java
@@ -1,13 +1,24 @@
 package exception;
 
+import java.util.Scanner;
+
 public class TesteMatematica {
-	public static void main(String[] args) throws NumeroInvalidoException {
+	public static void main(String[] args) {
+		
+	
+	int valor, gerar;
+	Scanner ler = new Scanner(System.in);
+	do {
 		try {
-			int retorno;
-		}catch (NumeroInvalidoException e ){
+			System.out.println("Digite um valor: ");
+			valor = ler.nextInt();
+			gerar = Matematica.gerarNum(valor);
+			break;
+		}catch(NumeroInvalidoException e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
+			System.out.println(e.getCodigoErro());
 		}
-	}
-
+	}while(true);
+}
 }
