fa5dc8a08a157d7ccc03249d17c3383561a32614
Victor-sarti <Sartivictor24@gmail.com>
2024-10-01 09:02:52 -0300

    add construtor and metodo in class No

--- src/lista/No.java
+++ src/lista/No.java
@@ -1,5 +1,39 @@
 package lista;
 
-public class No {
+public class No<Tipo> {
+	private Tipo elemento;
+	private No<Tipo> proximo;
 
+	public No(Tipo elemento) {
+		this.elemento = elemento;
+		this.proximo = null;
+	}
+		
+	public No(Tipo elemento, No<Tipo> proximo) {
+		this.elemento = elemento;
+		this.proximo = proximo;
+	}
+	
+	public Tipo getElemento() {
+		return elemento;
+	}
+
+	public void setElemento(Tipo elemento) {
+		this.elemento = elemento;
+	}
+
+	public No<Tipo> getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(No<Tipo> proximo) {
+		this.proximo = proximo;
+	}
+
+	@Override
+	public String toString() {
+		return "No [elemento=" + elemento + ", proximo=" + proximo + "]";
+	}
+	
+	
 }
