c19414e678bddc8953bf9e9debd72b56732cb8c7
Victor-sarti <Sartivictor24@gmail.com>
2024-10-01 08:47:35 -0300

    create metodo construtor

--- src/lista/ListaEncadeada.java
+++ src/lista/ListaEncadeada.java
@@ -1,5 +1,43 @@
 package lista;
 
-public class ListaEncadeada {
+public class ListaEncadeada<Tipo> {
+	private No inicio;
+	private No fim;
+	private int tamanho;
 
+	// metodo construtor
+
+	public ListaEncadeada() {
+		this.tamanho = 0;
+	}
+
+	public No getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(No inicio) {
+		this.inicio = inicio;
+	}
+
+	public No getFim() {
+		return fim;
+	}
+
+	public void setFim(No fim) {
+		this.fim = fim;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	// metodo exibicao
+	@Override
+	public String toString() {
+		return "ListaEncadeada [inicio=" + inicio + "]";
+	}
 }
