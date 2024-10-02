0c6e9b2c578a20212115842e659aae5dd453dde8
Victor-sarti <sartivictor24@gmail.com>
2024-10-02 08:57:35 -0300

    Create Exercicio2JFrame

--- src/pi/ExercicioJFrame2Cadastro.java
+++ src/pi/ExercicioJFrame2Cadastro.java
@@ -6,20 +6,27 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class TesteJFrame2 extends JFrame implements ActionListener {
+public class ExercicioJFrame2Cadastro extends JFrame implements ActionListener {
 
-	JLabel nome;
-	JTextField txtNome;
+	JLabel cpf, nome;
+	JTextField txtCpf, txtNome;
 	JButton btnGravar, btnSair;
 
-	public TesteJFrame2() {
-		setTitle("Cadastro");// Titulo da janela
-		setSize(400, 300); // tamanho da janela
+	public ExercicioJFrame2Cadastro() {
+		//Selecionando a Paleta de cores no swing
+				Color c = JColorChooser.showDialog(null,  
+						"Escolha uma cor", Color.lightGray);
+				getContentPane().setBackground(c);
+		
+		setTitle("Primeira Janela Swing");// Titulo da janela
+		setSize(600, 400); // tamanho da janela
 		setLayout(null);// anula o layout padrao
 		int alt = 40;
-		getContentPane().setBackground(new Color(0, 0, 0));
-		nome = criarJLabel("Nome", 10, 10, 270, alt);
-		txtNome = criarTexto(10, 50, 270, alt);
+		cpf = criarJLabel("CPF:", 10, 10, 270, alt);
+		nome = criarJLabel("NOME:", 10, 95, 270, alt);
+
+		txtCpf = criarTexto(10, 50, 270, alt);
+		txtNome = criarTexto(10, 132, 270, alt);
 		int rodape = getHeight() - 80;
 		int distEsq = getWidth() / 2;
 		int larg = 150;
@@ -39,7 +46,7 @@ public class TesteJFrame2 extends JFrame implements ActionListener {
 		b1.setToolTipText("Botão " + string);
 		b1.setHorizontalAlignment(0);
 		b1.setVerticalAlignment(0);
-//					b1.setEnabled(false); //Botão desabilitado.
+//	b1.setEnabled(false); //Botão desabilitado.
 		b1.setMnemonic(c); // Tecla de atalho
 		b1.addActionListener(this);
 		add(b1);
@@ -50,30 +57,30 @@ public class TesteJFrame2 extends JFrame implements ActionListener {
 		JTextField jt = new JTextField();
 		jt.setBounds(desq, dtopo, larg, alt);
 		jt.setForeground(Color.blue);
-//					jt.setText("Insira texto aqui");
-//					jt.setHorizontalAlignment(4);
+//	jt.setText("Insira texto aqui");
+//	jt.setHorizontalAlignment(4);
 		add(jt);
 		return jt;
 	}
 
 	public JLabel criarJLabel(String string, int desq, int dtopo, int larg, int alt) {
 		JLabel jl = new JLabel(string);
-//					jl.setText("JLabel inserido");
+//	jl.setText("JLabel inserido");
 		jl.setLocation(desq, dtopo); // dist esq e topo
 		jl.setSize(larg, alt); // larg e alt do rótulo
-//					jl.setOpaque(true);		//fundo opcao
-//					jl.setBackground(new Color(255,255,255)); //cor fundo
+//	jl.setOpaque(true);		//fundo opcao
+//	jl.setBackground(new Color(255,255,255)); //cor fundo
 		jl.setForeground(new Color(255, 255, 255)); // cor fonte
 		jl.setFont(new Font("Courier new", Font.BOLD, 18));// nome, estilo e tamanho da fonte
-//					jl.setToolTipText("JLabel Exemplo"); //dica de texto
-//					jl.setHorizontalAlignment(SwingConstants.LEFT);//alinhamento horizontal
-//					jl.setVerticalAlignment(SwingConstants.TOP);//alinhamento vertical
+//	jl.setToolTipText("JLabel Exemplo"); //dica de texto
+//	jl.setHorizontalAlignment(SwingConstants.LEFT);//alinhamento horizontal
+//	jl.setVerticalAlignment(SwingConstants.TOP);//alinhamento vertical
 		add(jl);
 		return jl;
 	}
 
 	public static void main(String[] args) {
-		new TesteJFrame2();
+		new ExercicioJFrame2Cadastro();
 	}
 
 	public void centralizar() {
