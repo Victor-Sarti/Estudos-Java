ba2baa82a1b4875368ce6404fabbcabfcd7e5343
Victor-Sarti <sartivictor24@gmail.com>
2024-10-09 08:35:08 -0300

    create Atalho

--- src/pi/TesteBorderLayout.java
+++ src/pi/TesteBorderLayout.java
@@ -13,15 +13,22 @@ public class TesteBorderLayout extends JFrame{
 		
 		BorderLayout bl = new BorderLayout(5,5);		
 		c.setLayout(bl);
-		
-		JButton b1 = criarBotao("Botao Norte" , BorderLayout.NORTH);
-		JButton b2 = criarBotao("Botao Sul" , BorderLayout.SOUTH);
-		JButton b3 = criarBotao("Botao Leste", BorderLayout.EAST);
-		JButton b4 = criarBotao("Botao Oeste", BorderLayout.WEST);
-		JButton b5 = criarBotao("Botao Centro", BorderLayout.CENTER );
+		//segure o ALT + letra
+		JButton b1 = criarBotao("Botao Norte" , BorderLayout.NORTH, 'N');
+		JButton b2 = criarBotao("Botao Sul" , BorderLayout.SOUTH, 'S');
+		JButton b3 = criarBotao("Botao Leste", BorderLayout.EAST, 'L');
+		JButton b4 = criarBotao("Botao Oeste", BorderLayout.WEST, 'O');
+		JButton b5 = criarBotao("Botao Centro", BorderLayout.CENTER, 'C');
 		setVisible(true);		
 	}
 	
+	private JButton criarBotao(String texto, String posicao, char c) {
+		JButton b1 = new JButton(texto);
+		b1.setMnemonic(c);
+		add(b1,posicao);
+		return b1;
+	}
+
 	private JButton criarBotao(String texto, String posicao) {
 		JButton b1 = new JButton(texto);
 		add(b1, posicao);
