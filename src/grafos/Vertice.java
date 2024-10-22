8d244c88b2d4d1cfe475030c0fa0f51f2aeab3ef
victor-sarti <sartivictor24@gmail.com>
2024-10-22 10:44:31 -0300

    get and set vertice

--- src/grafos/Vertice.java
+++ src/grafos/Vertice.java
@@ -14,5 +14,35 @@ public class Vertice <TIPO> {
 		this.arestasEntrada = new ArrayList<Aresta<TIPO>>();
 		this.arestasSaida = new ArrayList<Aresta<TIPO>>();
 	}
+
+
+	public TIPO getDado() {
+		return dado;
+	}
+
+
+	public void setDado(TIPO dado) {
+		this.dado = dado;
+	}
+
+
+	public ArrayList<Aresta<TIPO>> getArestasEntrada() {
+		return arestasEntrada;
+	}
+
+
+	public void setArestasEntrada(ArrayList<Aresta<TIPO>> arestasEntrada) {
+		this.arestasEntrada = arestasEntrada;
+	}
+
+
+	public ArrayList<Aresta<TIPO>> getArestasSaida() {
+		return arestasSaida;
+	}
+
+
+	public void setArestasSaida(ArrayList<Aresta<TIPO>> arestasSaida) {
+		this.arestasSaida = arestasSaida;
+	}
 	
 }
