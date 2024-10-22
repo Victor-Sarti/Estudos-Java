8ef1ffb430f074a8cd2f7b26a7705de391a394d1
victor-sarti <sartivictor24@gmail.com>
2024-10-22 11:01:08 -0300

    add construct adicionarAresta

--- src/grafos/Grafo.java
+++ src/grafos/Grafo.java
@@ -18,6 +18,27 @@ public class Grafo <TIPO> {
 		this.vertices.add(novoVertice);
 	}
 	
+	public void adicionarAresta (Double peso, TIPO dadoInicio, TIPO dadoFim) {
+		Vertice<TIPO> inicio = this.getVertice(dadoInicio);
+		Vertice<TIPO> fim = this.getVertice(dadoFim);
+		Aresta<TIPO> aresta = new Aresta<TIPO>(peso,inicio,fim);
+		inicio.adicionarArestaSaida(aresta);
+		fim.adicionarArestaEntrada(aresta);
+		this.arestas.add(aresta);
+	}
+
+	private Vertice<TIPO> getVertice(TIPO dado) {
+	Vertice<TIPO> vertice = null;
+	for(int i=0; i < this.vertices.size(); i++) {
+		if(this.vertices.get(i).getDado().equals(dado)) {
+			vertice = this.vertices.get(i);
+			break;
+		}
+	}
+	return vertice;
+		
+	}
+	
 
 	
 	
