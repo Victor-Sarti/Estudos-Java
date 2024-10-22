9786090f58daaf13ec4393e188e0f77d8359e320
victor-sarti <sartivictor24@gmail.com>
2024-10-22 10:49:41 -0300

    add class Grafo

---
+++ src/grafos/Grafo.java
@@ -0,0 +1,22 @@
+package grafos;
+
+import java.util.ArrayList;
+
+public class Grafo <TIPO> {
+	
+	private ArrayList<Vertice<TIPO>>vertices;
+	private ArrayList<Aresta<TIPO>> arestas;
+	
+	
+	public Grafo() {
+		this.vertices = vertices;
+		this.arestas = arestas;
+	}
+	
+	
+	
+
+	
+	
+	
+}
