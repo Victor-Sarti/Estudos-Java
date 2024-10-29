1045f5a54e6f83327b10e68ca20596d1cf0af87e
victor-sarti <sartivictor24@gmail.com>
2024-10-29 09:23:50 -0300

    add metodo preOrder

--- src/arvores/Arvore.java
+++ src/arvores/Arvore.java
@@ -1,26 +1,38 @@
 package arvores;
 
 public class Arvore {
-No root = null;
+	No root = null;
 
-public void insert(int info, No place) {
-	if(place == null) {
-		root = new No(info);
-		System.out.println(" " + info);
-	}else if(info < place.info) {
-		if(place.left == null) {
-			place.left = new No(info);
+	public void insert(int info, No place) {
+		if (place == null) {
+			root = new No(info);
 			System.out.println(" " + info);
-		}else {
-			insert(info,place.left);
-		}
-	}else if(info > place.info) {
-		if(place.right == null) {
-			place.right = new No(info);
-			System.out.println(" " +info);
-		}else {
-			insert(info, place.right);
+		} else if (info < place.info) {
+			if (place.left == null) {
+				place.left = new No(info);
+				System.out.println(" " + info);
+			} else {
+				insert(info, place.left);
+			}
+		} else if (info > place.info) {
+			if (place.right == null) {
+				place.right = new No(info);
+				System.out.println(" " + info);
+			} else {
+				insert(info, place.right);
+			}
 		}
 	}
-}
+
+	public void preOrder(No place) {
+		System.out.println(" " + place.info);
+		if (place.left != null) {
+			preOrder(place.left);
+		}
+		if (place.right != null) {
+			preOrder(place.right);
+		}
+	}
+	
+	
 }
